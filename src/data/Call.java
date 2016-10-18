6a4b090285e328190af4c26f4cbfd2e719285cd1
[CYBOR] <muhametshin.1997@gmail.com>
2016-10-18 21:23:35 +0300

    Filled base classes

--- src/data/Call.java
+++ src/data/Call.java
@@ -1,5 +1,14 @@
 package data;
 
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+
 public class Call
 {
+    private int id;
+    private DateTime date;
+    private Duration duration;
+    private boolean incoming;
+    private long phoneNumber;
+    private String comment;
 }
--- src/data/Departure.java
+++ src/data/Departure.java
@@ -1,5 +1,12 @@
 package data;
 
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+
 public class Departure
 {
+    private int id;
+    private DateTime date;
+    private Duration duration;
+    private String address, result;
 }
