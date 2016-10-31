bef43b6ba251ee7c03e9e6d7a22d51b4d613dcb7
[CYBOR] <muhametshin.1997@gmail.com>
2016-11-01 00:54:29 +0300

    Fixed unsecure operation.

--- src/data_exchange/RequestProcessor.java
+++ src/data_exchange/RequestProcessor.java
@@ -11,54 +11,55 @@ public class RequestProcessor
     public static Response process(Request request)
     {
         Response result = new Response();
-        switch (request.getType())
-        {
-            case "TEST":
-                return new Response("OK");
-            case "GET":
-                switch (request.in())
-                {
-                    case "Call":
-                        return new Response(dbHolder.getCalls());
-                    case "Departure":
-                        return new Response(dbHolder.getDepartures());
-                }
-                break;
-            case "ADD":
-                switch (request.in())
-                {
-                    case "Call":
-                        dbHolder.addCall(Call.parse(request.getData()));
-                        break;
-                    case "Departure":
-                        dbHolder.addDeparture(Departure.parse(request.getData()));
-                        break;
-                }
-                return new Response("OK");
-            case "SET":
-                switch (request.in())
-                {
-                    case "Call":
-                        dbHolder.setCall(Call.parse(request.getData()));
-                        break;
-                    case "Departure":
-                        dbHolder.setDeparture(Departure.parse(request.getData()));
-                        break;
-                }
-                return new Response("OK");
-            case "DELETE":
-            case "REMOVE":
-                switch (request.in())
-                {
-                    case "Call":
-                        dbHolder.removeCall(Call.parse(request.getData()));
-                        break;
-                    case "Departure":
-                        dbHolder.removeDeparture(Departure.parse(request.getData()));
-                        break;
-                }
-                return new Response("OK");
-        }
+        if (request.getType() != null)
+            switch (request.getType())
+            {
+                case "TEST":
+                    return new Response("OK");
+                case "GET":
+                    switch (request.in())
+                    {
+                        case "Call":
+                            return new Response(dbHolder.getCalls());
+                        case "Departure":
+                            return new Response(dbHolder.getDepartures());
+                    }
+                    break;
+                case "ADD":
+                    switch (request.in())
+                    {
+                        case "Call":
+                            dbHolder.addCall(Call.parse(request.getData()));
+                            break;
+                        case "Departure":
+                            dbHolder.addDeparture(Departure.parse(request.getData()));
+                            break;
+                    }
+                    return new Response("OK");
+                case "SET":
+                    switch (request.in())
+                    {
+                        case "Call":
+                            dbHolder.setCall(Call.parse(request.getData()));
+                            break;
+                        case "Departure":
+                            dbHolder.setDeparture(Departure.parse(request.getData()));
+                            break;
+                    }
+                    return new Response("OK");
+                case "DELETE":
+                case "REMOVE":
+                    switch (request.in())
+                    {
+                        case "Call":
+                            dbHolder.removeCall(Call.parse(request.getData()));
+                            break;
+                        case "Departure":
+                            dbHolder.removeDeparture(Departure.parse(request.getData()));
+                            break;
+                    }
+                    return new Response("OK");
+            }
         return result;
     }
 }
